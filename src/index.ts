210b7237b015fcf2193d95658160b2996bd32aa9
lucasKuratani <lucaskuratani.dev@gmail.com>
2021-09-08 10:05:21 -0300

    Added linting config and initial file.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-export const getRev = () => 'dsad'
+import { startOfMonth, add, isSameWeek } from 'date-fns'
+
+export const getRev = (date: Date): number => {
+  const firstDayOfDateMonth = startOfMonth(date)
+  const firstDayOfNextMonth = startOfMonth(add(date, { months: 1 }))
+
+  if (isSameWeek(date, firstDayOfDateMonth, { weekStartsOn: 6 }) || isSameWeek(date, firstDayOfNextMonth, { weekStartsOn: 6 })) return 0
+
+  return 1
+}
