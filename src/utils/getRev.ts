e2b471dc81ae18750b2f3acf0766ec913c4a3bef
lucasKuratani <lucaskuratani.dev@gmail.com>
2021-10-08 20:43:01 +0000

    Removed console.log method.

--- src/utils/getRev.ts
+++ src/utils/getRev.ts
@@ -5,8 +5,6 @@ export const getRev = (date: Date): number => {
   const startOfMonthLastRev = add(startOfWeek(startOfMonth(add(date, { months: 1 })), { weekStartsOn: 6 }), { weeks: -1 })
   const startsOfAllMonthRevs = eachWeekOfInterval({ start: startOfMonthFirstRev, end: startOfMonthLastRev }, { weekStartsOn: 6 })
 
-  console.log(startsOfAllMonthRevs)
-
   if (isSameWeek(startOfWeek(startOfMonth(add(date, { months: 1 })), { weekStartsOn: 6 }), date, { weekStartsOn: 6 })) return 0
 
   return startsOfAllMonthRevs.findIndex(startOfRev => isSameWeek(startOfRev, date, { weekStartsOn: 6 }))
